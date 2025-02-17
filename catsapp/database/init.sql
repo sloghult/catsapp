e2631202238f43971dd20560d53756de385c507f
sloghult <jvalentin@guardiaschool.fr>
2025-02-17 21:56:19 +0100

    réglages pb /admin

--- catsapp/database/init.sql
+++ catsapp/database/init.sql
@@ -23,9 +23,3 @@ CREATE TABLE IF NOT EXISTS messages (
     FOREIGN KEY (sender_id) REFERENCES users(id),
     FOREIGN KEY (receiver_id) REFERENCES users(id)
 );
-
--- Insertion des utilisateurs de test
--- Note: Les mots de passe sont hachés avec Werkzeug (équivalent à '123')
-INSERT INTO users (username, password, nom, prenom) VALUES
-('admin', 'pbkdf2:sha256:600000$8qPYhxKJxQi1Sirx$6e0a85d6f94aa66667e2f1c10e5e4d4a48a9d3c50c1b3e2352c4e9671b6b89e9', 'Admin', 'Admin')
-ON DUPLICATE KEY UPDATE username=username;
