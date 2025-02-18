9f42f837f23d657f392ec086a6da24af65368739
W1tchC@t? <yui.mhcp01@gmail.com>
2025-02-18 11:13:28 +0100

    delete all css

--- catsapp/add_user.py
+++ catsapp/add_user.py
@@ -1,25 +1,29 @@
-from database.db import DB_CONFIG
+from database.db import DB_CONFIG, user_exists
 import mysql.connector
 from werkzeug.security import generate_password_hash
 
-def add_user():
+def add_user(username, password, nom, prenom):
     connection = mysql.connector.connect(**DB_CONFIG, database='catsapp')
     try:
         cursor = connection.cursor()
-        
+
+        # Vérifier si l'utilisateur existe déjà
+        if user_exists(username):
+            print("Ce nom d'utilisateur est déjà pris. Veuillez en choisir un autre.")
+            return False
+
         # Générer le hash du mot de passe
-        hashed_password = generate_password_hash('123')
-        
+        hashed_password = generate_password_hash(password)
+
         # Insérer le nouvel utilisateur
         sql = "INSERT INTO users (username, password, nom, prenom) VALUES (%s, %s, %s, %s)"
-        values = ('pierre', hashed_password, 'Dupont', 'Pierre')
-        
-        cursor.execute(sql, values)
+        cursor.execute(sql, (username, hashed_password, nom, prenom))
         connection.commit()
-        print("Utilisateur Pierre ajouté avec succès!")
-        
-    except Exception as e:
-        print(f"Erreur: {e}")
+        print("Utilisateur ajouté avec succès.")
+        return True
+    except mysql.connector.Error as err:
+        print(f"Erreur: {err}")
+        return False
     finally:
         cursor.close()
         connection.close()
